9916604b2dc4d2271b6d283cc8b4226925da1ee1
Razvan Sandru <razvansandru93@gmail.com>
2023-02-14 09:26:39 +0200

    Added Archive model Added file upload/download elements Removed Privacy logic

---
+++ TestRS.Core/Archive.cs
@@ -0,0 +1,10 @@
+﻿namespace TestRS.Core;
+public class Archive
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; }
+    public DateTime UploadedDate { get; set; }
+    public int ArchiveTime { get; set; }
+    public bool Status { get; set; }
+    public byte[] Data { get; set; }
+}
--- TestRS.Web/Controllers/HomeController.cs
+++ TestRS.Web/Controllers/HomeController.cs
@@ -17,12 +17,13 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public async Task<IActionResult> UploadFile(IFormFile file)
     {
-        return View();
+        return View("GetArchive");
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
