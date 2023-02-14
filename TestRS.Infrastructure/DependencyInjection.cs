87f4244d5f86d8e8e04ecaaabc761a5474217f0c
Razvan Sandru <razvansandru93@gmail.com>
2023-02-14 11:10:42 +0200

    Added DapperContext Added ArchiveRepository Add logic

--- TestRS.Infrastructure/DependencyInjection.cs
+++ TestRS.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using TestRS.Core.Repositories;
 using TestRS.Core.Services;
+using TestRS.Infrastructure.Context;
 using TestRS.Infrastructure.Repositories;
 using TestRS.Infrastructure.Services;
 
@@ -11,6 +12,7 @@ public static class DependencyInjection
     {
         services.AddScoped<IArchiveRepository, ArchiveRepository>();
         services.AddScoped<IArchiverService, ArchiverService>();
+        services.AddSingleton<DapperContext>();
 
         return services;
     }
--- TestRS.Infrastructure/Repositories/ArchiveRepository.cs
+++ TestRS.Infrastructure/Repositories/ArchiveRepository.cs
@@ -1,12 +1,48 @@
-﻿using TestRS.Core;
+﻿using Dapper;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using TestRS.Core;
 using TestRS.Core.Repositories;
+using TestRS.Infrastructure.Context;
 
 namespace TestRS.Infrastructure.Repositories;
 public class ArchiveRepository : IArchiveRepository
 {
-    public Task Add(Archive archive)
+    private readonly DapperContext _dapperContext;
+    private readonly ILogger<ArchiveRepository> _logger;
+
+    public ArchiveRepository(DapperContext dapperContext, ILogger<ArchiveRepository> logger)
     {
-        throw new NotImplementedException();
+        _dapperContext = dapperContext;
+        _logger = logger;
+    }
+
+    public async Task Add(Archive archive)
+    {
+        _logger.LogInformation($"Started adding archive with id: {archive.Id} to database");
+        var query = "INSERT INTO [RS].[dbo].[Archives] (Id, FileName, UploadedDate, ArchiveTimeInMiliseconds, Status, Data) " +
+                "VALUES (@Id, @FileName, @UploadedDate, @ArchiveTimeInMiliseconds, @Status, @Data)";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("Id", archive.Id, DbType.Guid);
+        parameters.Add("FileName", archive.FileName, DbType.String);
+        parameters.Add("UploadedDate", archive.UploadedDate, DbType.DateTime);
+        parameters.Add("ArchiveTimeInMiliseconds", archive.ArchiveTime, DbType.Int32);
+        parameters.Add("Status", archive.Status, DbType.Boolean);
+        parameters.Add("Data", archive.Data, DbType.Binary);
+
+        try
+        {
+            using var connection = _dapperContext.CreateConnection();
+            await connection.ExecuteAsync(query, parameters);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.ToString());
+            throw;
+        }
+
+        _logger.LogInformation($"Finished adding archive with id: {archive.Id} to database");
     }
 
     public Task<Archive?> GetArchiveById(Guid archiveId)
